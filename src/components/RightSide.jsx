907e79b51dd1cb2d70f794ffd02f6cf685f359fb
Phuc Le <phuclevinh2000@gmail.com>
2021-12-31 05:07:41 +0200

    working on rightside and leftside

--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
+import React from 'react';
 
-import styled from 'styled-components'
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
 
 const RightSide = () => {
   return (
     <Container>
-      RightSide
-    </Container>
-  )
-}
+      <FollowCard>
+        <Title>
+          <h2>Add to your feed</h2>
+        </Title>
 
-export default RightSide
+        <FeedList>
+          <li>
+            <Link to=''>hi</Link>
+          </li>
+        </FeedList>
+      </FollowCard>
+    </Container>
+  );
+};
+
+export default RightSide;
 
 const Container = styled.div`
   grid-area: rightside;
-`
+`;
 
+const FollowCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 /15%), 0 0 0 rgb(0 0 0 / 20%);
+  padding: 12px;
+`;
+
+const Title = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  width: 100%;
+  color: rgba(0, 0, 0, 0.6);
+`;
+
+const FeedList = styled.ul``;
