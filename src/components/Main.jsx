08a5217ffbf877b1d28570bcc1103b8314a047c7
Phuc Le <phuclevinh2000@gmail.com>
2022-01-01 05:27:05 +0200

    working on main and rightside component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Main = () => {
   return (
     <Container>
-      Main
+      <ShareBox>Share</ShareBox>
     </Container>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
 
 const Container = styled.div`
   grid-area: main;
-`
+`;
 
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 /15%), 0 0 0 rgb(0 0 0 / 20%);
+`;
+
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b95;
+  margin: 0 0 8px;
+  background: white;
+`;
--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -10,14 +10,40 @@ const RightSide = () => {
       <FollowCard>
         <Title>
           <h2>Add to your feed</h2>
+          <img src='/images/feed-icon.svg' alt='' />
         </Title>
 
         <FeedList>
           <li>
-            <Link to=''>hi</Link>
+            <Link to=''>
+              <Avatar />
+            </Link>
+            <div>
+              <span>#Linkedin</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+            <Link to=''>
+              <Avatar />
+            </Link>
+            <div>
+              <span>#Video</span>
+              <button>Follow</button>
+            </div>
           </li>
         </FeedList>
+        <Recommmendation>
+          View all recommendation
+          <img src='/images/right-icon.svg' alt='' />
+        </Recommmendation>
       </FollowCard>
+      <BannerCard>
+        <img
+          src='https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg'
+          alt=''
+        />
+      </BannerCard>
     </Container>
   );
 };
@@ -26,6 +52,7 @@ export default RightSide;
 
 const Container = styled.div`
   grid-area: rightside;
+  margin-top: -15px;
 `;
 
 const FollowCard = styled.div`
@@ -49,4 +76,59 @@ const Title = styled.div`
   color: rgba(0, 0, 0, 0.6);
 `;
 
-const FeedList = styled.ul``;
+const FeedList = styled.ul`
+  margin-top: 16px;
+  li {
+    display: flex;
+    align-items: center;
+    margin: 12px 0;
+    position: relative;
+    font-size: 14px;
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+
+  button {
+    background-color: transparent;
+    color: rgba(0, 0, 0, 0.6);
+    box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.6);
+    padding: 16px;
+    border-radius: 15px;
+    align-items: center;
+    box-sizing: border-box;
+    font-weight: 600;
+    display: inline-flex;
+    justify-content: center;
+    max-height: 32px;
+    max-width: 480px;
+    text-align: center;
+    outline: none;
+  }
+`;
+
+const Avatar = styled.div`
+  background-image: url('https://static-exp1.licdn.com/sc/h/1b4vl1r54ijmrmcyxzoidwmxs');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 48px;
+  height: 48px;
+  margin-right: 8px;
+`;
+
+const Recommmendation = styled.a`
+  color: #0a66c2;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+`;
+
+const BannerCard = styled(FollowCard)`
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
